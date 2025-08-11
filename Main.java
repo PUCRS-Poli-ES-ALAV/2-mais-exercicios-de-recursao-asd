be0a4a1aed44c1f9691d9284f83ef5a8c0379d9f
Caio Ferreira <141863391+caschferr@users.noreply.github.com>
2025-08-11 19:52:48 -0300

    toBinary e permutations

--- Main.java
+++ Main.java
@@ -4,22 +4,25 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("Forneca um numero: ");
-        int n = sc.nextInt();
-        System.out.println(nroDigit(n));
+        System.out.print("Forneca uma string: ");
+        String s = sc.nextLine();
+        System.out.println(permutations(s));
         sc.close();
     }
 
+    @SuppressWarnings("unused")
     private static int fatorial (int n) {
         if (n <= 1) return 1;
         return n * fatorial (n-1);
     }
 
+    @SuppressWarnings("unused")
     private static int somatorio (int n) {
         if (n == 0) return 0;
         return n + somatorio (n - 1);
     }
 
+    @SuppressWarnings("unused")
     private static int fibonacci (int n) {
         if (n < 0) return -1;
         if (n == 0) return 0;
@@ -27,6 +30,7 @@ public class Main {
         return fibonacci(n-1) + fibonacci(n-2);
     }
 
+    @SuppressWarnings("unused")
     private static int somatorioEntre (int n, int m) {
         if (n == m) return m;
         if (n > m) return 0;
@@ -35,22 +39,26 @@ public class Main {
         return t + somatorioEntre(n+1, m-1);
     }
 
+    @SuppressWarnings("unused")
     private static boolean isPal (String s) {
         if (s.length() <= 1) return true;
         if (s.charAt(0) != (s.charAt(s.length()-1))) return false;
         return isPal(s.substring(1, s.length()-1));
     }
 
+    @SuppressWarnings("unused")
     private static String toBinary (int n) {
-        return "";
-        // nah
+        if (n < 2) return String.valueOf(n);
+        return toBinary(n/2) + (n%2);
     }
 
+    @SuppressWarnings("unused")
     private static int alSum (ArrayList<Integer> al) {
         int current = al.remove(0);
         return current + alSum(al);
     }
 
+    @SuppressWarnings("unused")
     private static int findBiggest (ArrayList<Integer> al) {
         if (al.isEmpty()) return -1;
         if (al.size() == 1) return al.get(0);
@@ -61,19 +69,38 @@ public class Main {
         return findBiggest(al);
     }
 
+    @SuppressWarnings("unused")
     private static boolean findSubstring (String s, String match) {
         if (s.length() < match.length()) return false;
         if (s.startsWith(match)) return true;
         return findSubstring(s.substring(1), match);
     }
 
+    @SuppressWarnings("unused")
     private static int nroDigit (int n) {
         if (n < 10) return 1;
         int remaining = n / 10;
         return 1 + nroDigit (remaining);
     }
 
+    @SuppressWarnings("unused")
     private static ArrayList<String> permutations (String s) {
-        return null;
+        ArrayList<String> perms = new ArrayList<>();
+
+        if (s.length() == 1) {
+            perms.add(s);
+            return perms;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            String rest = s.substring(0,i) + s.substring(i+1);
+
+            ArrayList<String> currentPerms = permutations(rest);
+
+            for (String p : currentPerms) perms.add(current + p);
+        }
+
+        return perms;
     }
 }
